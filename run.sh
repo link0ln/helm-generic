f3eebea0de41c472a4c3c9568e5ce341289816fc
sshiktarev <sshiktarev@web3tech.ru>
2022-11-16 12:17:19 +0000

    update -a

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+helm lint ./charts/generic-service
+
+helm package ./charts/generic-service
+
+helm repo index --url https://link0ln.github.io/helm-generic/ .
+
+git add .
+
+git commit -m "update" -a
+
+git push
